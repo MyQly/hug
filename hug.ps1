ce9bd5350bd8369adbfadf799c3ff5bae9690d07
Mike <github@myqly.com>
2022-02-25 07:32:17 -0600

    Updated options, removed some legacy cmd-based code and added automation icon application to the generated exe with rcedit (I want to remove this exe from the repo later).

--- hug.ps1
+++ hug.ps1
@@ -1,33 +1,65 @@
 # hug
 #
 # Usage: .\hug.ps1 GameFolder zs
-# Parameter : Game Folder (where main.lua lives, can be a directory outside of Love2D installation path)
-# z - retain the generated .zip file
-# s - silent (do *not* run the exe after packaging)
+# Required : Game Folder (where main.lua lives, can be a directory outside of Love2D installation path)
+# Optional : z - retain the generated .zip file
+# Optional : r - will execute the game after building
+# Optional : i - will execute rcedit-x64 to update the exe icon 
 #
 # Game.exe will remain in the love directory.
+
+$deleteZip = $true
+$runGame = $false
+$updateIcon = $false
 
 $argCount = $args.Count
 if ($argCount -lt 1 -or $argCount -gt 2) {
     Write-Host "Game folder name expected: e.g. .\hug.ps1 GameName"
 } else {
     $game=$args[0]
+    
     if (Get-Item .\$game.zip -ErrorAction SilentlyContinue) {
-        rm .\$game.zip
+        Remove-Item .\$game.zip
     }
     if (Get-Item .\$game.exe -ErrorAction SilentlyContinue) {
-        rm .\$game.exe
+        Remove-Item .\$game.exe
     }
+    
     Compress-Archive .\$game\* -DestinationPath .\$game.zip
+    
+    # TO-DO Find a PS equivalent to this call.
     cmd /c copy /b .\love.exe+.\$game.zip .\$game.exe
+
+    # If an icon exists under gfx/icon.ico apply it to the generated exe.
+    if (Get-Item .\$game\gfx\icon.ico -ErrorAction SilentlyContinue) {
+        Write-Host "Applying icon from gfx/icon.ico!"
+        # Run rcedit-x64 and update the newly created exe with our icon.
+        $p = Start-Process .\rcedit-x64.exe -ArgumentList " .\$game.exe --set-icon .\$game\gfx\icon.ico" -wait -NoNewWindow -PassThru
+        $p
+        # Wait for this process to finish before going any further to prevent locking the exe.
+        $p.HasExited
+    }
+    
     if ($argCount -eq 2) {
-        Write-Host $args[1]
-        $params=$args[1].ToLower()
-        if (-not($params -like '*z*')) {
-            rm .\$game.zip
+        $options=$args[1].ToLower()
+        if (($options -like '*z*')) {
+            $deleteZip = $false
         }
-        if (-not($params -like '*s*')) {
-            cmd /c "$game.exe"
+        if ($options -like '*r*') {
+            $runGame = $true
         }
+        if ($options -like '*i*') {
+            $updateIcon = $true
+        }
+    }
+
+    # If z option is not given or only 1 argument is found delete the .zip file.
+    if ($deleteZip) {
+        Remove-Item ".\$game.zip"
+    }
+
+    # If s option is not given or only 1 argument is found run the game
+    if ($runGame) {
+        Start-Process .\$game.exe
     }
 }
