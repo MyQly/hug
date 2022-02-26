e446c3bb8050d93bbc3910260a1d3c59a3d0c373
Mike <github@myqly.com>
2022-02-26 16:52:25 -0600

    Reorganized option flags. Removed cmd references with pure PS options.

--- hug.ps1
+++ hug.ps1
@@ -8,58 +8,74 @@
 #
 # Game.exe will remain in the love directory.
 
-$deleteZip = $true
-$runGame = $false
-$updateIcon = $false
-
 $argCount = $args.Count
+
+$options = @{
+    deleteZip = $true;
+    runGame = $false;
+    updateIcon = $false;
+}
+
 if ($argCount -lt 1 -or $argCount -gt 2) {
-    Write-Host "Game folder name expected: e.g. .\hug.ps1 GameName"
-} else {
-    $game=$args[0]
-    
-    if (Get-Item .\$game.zip -ErrorAction SilentlyContinue) {
-        Remove-Item .\$game.zip
-    }
-    if (Get-Item .\$game.exe -ErrorAction SilentlyContinue) {
-        Remove-Item .\$game.exe
-    }
-    
-    Compress-Archive .\$game\* -DestinationPath .\$game.zip
-    
-    # TO-DO Find a PS equivalent to this call.
-    cmd /c copy /b .\love.exe+.\$game.zip .\$game.exe
+    Write-Host "Argument count mismatch. Please use the following format: .\hug.ps1 GameName (zri)"
+    Exit
+}
 
-    # If an icon exists under gfx/icon.ico apply it to the generated exe.
-    if (Get-Item .\$game\gfx\icon.ico -ErrorAction SilentlyContinue) {
-        Write-Host "Applying icon from gfx/icon.ico!"
-        # Run rcedit-x64 and update the newly created exe with our icon.
-        $p = Start-Process .\rcedit-x64.exe -ArgumentList " .\$game.exe --set-icon .\$game\gfx\icon.ico" -wait -NoNewWindow -PassThru
-        $p
-        # Wait for this process to finish before going any further to prevent locking the exe.
-        $p.HasExited
-    }
-    
-    if ($argCount -eq 2) {
-        $options=$args[1].ToLower()
-        if (($options -like '*z*')) {
-            $deleteZip = $false
-        }
-        if ($options -like '*r*') {
-            $runGame = $true
-        }
-        if ($options -like '*i*') {
-            $updateIcon = $true
-        }
-    }
+$game=$args[0]
 
-    # If z option is not given or only 1 argument is found delete the .zip file.
-    if ($deleteZip) {
-        Remove-Item ".\$game.zip"
-    }
+if (-not(Get-Item .\$game -ErrorAction SilentlyContinue)) {
+    Write-Host "Game folder $game not found!"
+    Exit
+}
 
-    # If s option is not given or only 1 argument is found run the game
-    if ($runGame) {
-        Start-Process .\$game.exe
+
+
+if (Get-Item .\$game.zip -ErrorAction SilentlyContinue) {
+    Remove-Item .\$game.zip
+}
+if (Get-Item .\$game.exe -ErrorAction SilentlyContinue) {
+    Remove-Item .\$game.exe
+}
+
+# Generate .zip archive of game content from folder
+Compress-Archive .\$game\* -DestinationPath .\$game.zip
+# Generate game executable
+Add-Content -Path ".\$game.exe" -Value ((Get-Content -Path .\love.exe -Raw -AsByteStream) + (Get-Content -Path .\$game.zip -Raw -AsByteStream)) -AsByteStream
+
+
+if ($argCount -eq 2) {
+    $arg2=$args[1].ToLower()
+    if (($arg2 -like '*z*')) {
+        $options.deleteZip = $false
     }
+    if ($arg2 -like '*r*') {
+        $options.runGame = $true
+    }
+    if ($arg2 -like '*i*') {
+        $options.updateIcon = $true
+    }
+}
+
+# If an icon exists under gfx/icon.ico apply it to the generated exe.
+# I don't like using this .exe to do this job and the results seem inconsistent for some reason.
+if ((Get-Item .\$game\gfx\icon.ico -ErrorAction SilentlyContinue) -and $options.updateIcon) {
+    Write-Host "Applying icon from gfx/icon.ico!"
+    # Run rcedit-x64 and update the newly created exe with our icon.
+    $p = Start-Process .\rcedit-x64.exe -ArgumentList " .\$game.exe --set-icon icon.ico" -wait -NoNewWindow -PassThru
+    # Wait for this process to finish before going any further to prevent locking the exe.
+    while (-not($p.HasExited)) {
+        # This never actually renders from my tests but may if the system is slower.
+        Write-Host "." -NoNewLine
+    }
+}
+
+
+# If z option is not given or only 1 argument is found delete the .zip file.
+if ($options.deleteZip) {
+    Remove-Item ".\$game.zip"
+}
+
+# If s option is not given or only 1 argument is found run the game
+if ($options.runGame) {
+    Start-Process .\$game.exe
 }
